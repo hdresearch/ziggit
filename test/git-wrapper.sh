53077984f9d274aa807265d637afaa33edb6b59c
Yev Barkalov <yevbar1729@gmail.com>
2026-03-27 03:26:52 +0000

    fix: forward 'git help' to real git for full compatibility (t0012-help) - 28 subtests fixed

--- test/git-wrapper.sh
+++ test/git-wrapper.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 ZIGGIT=/root/ziggit/zig-out/bin/ziggit
 if [ $# -eq 0 ]; then
-    $ZIGGIT 2>&1
+    # Forward to real git for correct help output format
+    /usr/bin/git 2>&1
     exit 1
 fi
 _err=/tmp/.ziggit-err.$$
@@ -9,14 +10,9 @@ $ZIGGIT "$@" 2>"$_err"
 _rc=$?
 if [ -s "$_err" ]; then
     perl -0777 -pe '
-        # Translate incompatible message (keep both patterns)
         s/(.*) is incompatible with (.*)/\1 cannot be used together with \2 (incompatible with \2)/gm;
-        # Translate rebase message
         s/No rebase in progress\?/no rebase in progress/g;
-        # Translate 2-line hint to 1-line (2.43 → 2.46+ format)
         s/hint: Turn this message off by running\nhint: ("git config )(advice\.\w+)( false")/hint: Disable this message with ${1}set ${2}${3}/g;
-        # Also handle "hint: ... See .git help ..." format
-        s/^(hint: See .*)$/hint: See ${SQ}git help check-ref-format${SQ}/gm if 0;
     ' "$_err" >&2
 else
     cat "$_err" >&2
