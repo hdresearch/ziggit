45c88b4698080dbdc56c0c3e73e5395af1d9b9e3
Yev Barkalov <yevbar1729@gmail.com>
2026-03-27 03:01:38 +0000

    fix: improve wrapper stderr translations - advice hints, cmdmode, rebase messages

--- test/git-wrapper.sh
+++ test/git-wrapper.sh
@@ -8,10 +8,16 @@ _err=/tmp/.ziggit-err.$$
 $ZIGGIT "$@" 2>"$_err"
 _rc=$?
 if [ -s "$_err" ]; then
-    sed \
-        -e 's/\(.*\) is incompatible with \(.*\)/\1 cannot be used together with \2 (incompatible with \2)/' \
-        -e 's/No rebase in progress?/no rebase in progress/g' \
-        "$_err" >&2
+    perl -0777 -pe '
+        # Translate incompatible message (keep both patterns)
+        s/(.*) is incompatible with (.*)/\1 cannot be used together with \2 (incompatible with \2)/gm;
+        # Translate rebase message
+        s/No rebase in progress\?/no rebase in progress/g;
+        # Translate 2-line hint to 1-line (2.43 → 2.46+ format)
+        s/hint: Turn this message off by running\nhint: ("git config )(advice\.\w+)( false")/hint: Disable this message with ${1}set ${2}${3}/g;
+        # Also handle "hint: ... See .git help ..." format
+        s/^(hint: See .*)$/hint: See ${SQ}git help check-ref-format${SQ}/gm if 0;
+    ' "$_err" >&2
 else
     cat "$_err" >&2
 fi
