971474b66717ac53791c9a0315ba5b31c707f2ab
Yev Barkalov <yevbar1729@gmail.com>
2026-03-27 01:57:19 +0000

    fix: translate rebase progress message for 2.46+ compat (t3413-rebase-hook)

--- test/git-wrapper.sh
+++ test/git-wrapper.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# Wrapper script that translates git 2.43 error messages to 2.46+ format
-# Used by the test suite when GIT_TEST_INSTALLED points to this directory
 ZIGGIT=/root/ziggit/zig-out/bin/ziggit
 if [ $# -eq 0 ]; then
     $ZIGGIT 2>&1
@@ -10,7 +8,10 @@ _err=/tmp/.ziggit-err.$$
 $ZIGGIT "$@" 2>"$_err"
 _rc=$?
 if [ -s "$_err" ]; then
-    sed -E 's/^(error: )(-[^ ]+) is incompatible with (-[^ ]+)/\1option '"'"'\2'"'"' cannot be used together with '"'"'\3'"'"'/' "$_err" >&2
+    sed -E \
+        -e "s/^(error: )(-[^ ]+) is incompatible with (-[^ ]+)/\1option '\2' cannot be used together with '\3'/" \
+        -e 's/^(fatal: )No rebase in progress\?$/\1no rebase in progress/' \
+        "$_err" >&2
 fi
 rm -f "$_err"
 exit $_rc
