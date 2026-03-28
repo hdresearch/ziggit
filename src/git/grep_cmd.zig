5884c9ae19111555de1ff45255ecab15427f743c
Yev Barkalov <yevbar1729@gmail.com>
2026-03-28 01:19:22 +0000

    auto-save: agent progress

--- src/git/grep_cmd.zig
+++ src/git/grep_cmd.zig
@@ -91,6 +91,8 @@ const GrepOptions = struct {
             .pattern_files = std.array_list.Managed([]const u8).init(allocator),
             .extended_regexp_values = std.array_list.Managed(bool).init(allocator),
             .pattern_type_values = std.array_list.Managed(?PatternType).init(allocator),
+            .expr_tokens = std.array_list.Managed(ExprToken).init(allocator),
+            .expr_tokens_initialized = true,
         };
     }
 
